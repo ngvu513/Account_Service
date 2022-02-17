5b1ad5e66a74c6b7d90c92230007d86942713a8b
Ngoc Vu Nguyen <nvu135@gmail.com>
2022-02-17 17:42:14 -0500

    Init project

---
+++ Account Service/task/src/account/businesslayer/Authentication.java
@@ -0,0 +1,10 @@
+package account.businesslayer;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class Authentication {
+
+}
---
+++ Account Service/task/src/account/model/User.java
@@ -0,0 +1,8 @@
+package account.model;
+
+public class User {
+    String name;
+    String password;
+    String lastName;
+    String email;
+}
